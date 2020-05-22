895aff93c889e1461ea89b194d71705af8df9caf
azaika <takaaza@gmail.com>
2020-05-22 12:09:59 +0900

    remove unnecessary test

--- arrays/src/lib.rs
+++ arrays/src/lib.rs
@@ -7,13 +7,3 @@ pub use backing_array::*;
 pub use array_stack::*;
 pub use array_queue::*;
 pub use array_deque::*;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
